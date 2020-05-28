8fe4a86d5fd15103cb08392c5f467928154aaba8
unknown <venkat@pixelbrook.com>
2020-05-28 22:30:53 +0530

    Updates

---
+++ precache-manifest.6e8208b3c4dcc4397eedc64079a7f73c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d04d6f8c3df7733ebee5b14b4f1f3be",
+    "url": "/todo-posts/index.html"
+  },
+  {
+    "revision": "56fec7b8cb18bc32e02f",
+    "url": "/todo-posts/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "9323584b6fa55ed31d38",
+    "url": "/todo-posts/static/css/main.6ae6ca35.chunk.css"
+  },
+  {
+    "revision": "56fec7b8cb18bc32e02f",
+    "url": "/todo-posts/static/js/2.797f8321.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/todo-posts/static/js/2.797f8321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9323584b6fa55ed31d38",
+    "url": "/todo-posts/static/js/main.2fd5a0ac.chunk.js"
+  },
+  {
+    "revision": "d406f5991d4b1ad296eb",
+    "url": "/todo-posts/static/js/runtime-main.423f53d8.js"
+  }
+]);
